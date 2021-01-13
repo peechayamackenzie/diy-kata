0e33b18718fc6086e61cd66ae45fde751fe57ae2
Peechaya Mackenzie <tay_eee@hotmail.com>
2021-01-13 21:04:55 +0000

    amended code

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,8 +1,7 @@
 const joinNames = namesObj => {
-    const length = namesObj.length -1;
-    const firstname = namesObj.map(a => a.name).slice(0, 2).join(", ");
-    const lastname = namesObj[length].name;
-    return firstname + " & " + lastname
+    const first = namesObj.map(a => a.name).slice(0, 2).join(", ");
+    const last = namesObj.map(a => a.name).pop()
+    return first + " & " + last
 };
 
 module.exports = joinNames;
